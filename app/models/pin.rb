f028e6cf28037665f2adfb4872135ff630b0ca88
Callum Hopkins <callum.e.hopkins@gmail.com>
2015-09-25 09:50:13 +0100

    paperclip added to app

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,7 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
+
+	#attr_accessible()
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => 'placeholder-img_:style.jpg'
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
